0702b3ead2844354c06e15ddd73b9ee24a2933e4
pavlmitv <74025472+pavlmitv@users.noreply.github.com>
2021-06-04 23:13:46 +0300

    Add files via upload

---
+++ Workshop_MyWebServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Workshop_MyWebServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
